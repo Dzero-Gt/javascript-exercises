720c402043172c047fd3910a0076bc6c9e220418
Dzero-GT <dillongustafson2002@gmail.com>
2022-05-14 17:28:11 -0500

    exercise 4 complete

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,16 @@
-const removeFromArray = function() {
+const removeFromArray = function(arrayEx, place1, place2, place3, place4) {
+    for (let i = 0; i < arrayEx.length; i++){
+        if (arrayEx[i]===place1 || arrayEx[i]===place2 || arrayEx[i]===place3 || arrayEx[i]===place4){
+            arrayEx[i] = '*';
+        }
+    }
+    for (let i = 0; i < arrayEx.length; i++){
+        while (arrayEx[i]==='*'){
+            arrayEx.splice(i,1);
+        }
+    }
+    
+    return arrayEx;
 
 };
 
