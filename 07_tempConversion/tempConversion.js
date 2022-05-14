5fb8f264decebb0fdf444c66388c2babf6754576
Dzero-GT <dillongustafson2002@gmail.com>
2022-05-14 17:47:25 -0500

    exercise 7 complete

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,8 +1,21 @@
-const ftoc = function() {
-
+const ftoc = function(far) {
+  let celc = 0;
+  celc = far - 32;
+  celc = celc * ( 5 / 9);
+  celc *= 10;
+  celc =Math.round(celc);
+  celc /= 10;
+  return celc;
 };
 
-const ctof = function() {
+const ctof = function(celc) {
+  let far = 0;
+  far = celc * (9 / 5);
+  far += 32;
+  far *= 10;
+  far =Math.round(far);
+  far /= 10;
+  return far;
 
 };
 
