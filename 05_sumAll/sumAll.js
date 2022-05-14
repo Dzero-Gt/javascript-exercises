0b1c7469cd95cddbbeaf10ac570697ee36f68ba0
Dzero-GT <dillongustafson2002@gmail.com>
2022-05-14 17:35:52 -0500

    exercise 5 complete

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,19 @@
-const sumAll = function() {
-
+const sumAll = function(start, end) {
+    let results = 0;
+    if (start < 0 || end < 0 || typeof start != "number" || typeof end != "number"){
+        results = "ERROR";
+    }
+    else {
+        if (start > end){
+            let temp = start;
+            start = end;
+            end = temp;
+        }
+        for (let i = start; i <= end; i++){
+            results += i;
+        }
+    }
+    return results;
 };
 
 // Do not edit below this line
