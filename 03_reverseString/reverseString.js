28480bcb0584c0b416855ae4a7c4e90439969f5e
Dzero-GT <dillongustafson2002@gmail.com>
2022-05-14 17:07:22 -0500

    exercise 3 complete

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(text) {
+let results = "";
+let length = text.length;
+for (let i = length; i > 0; i--){
+    results += text.charAt(i - 1);
+}
+return results;
 };
 
 // Do not edit below this line
