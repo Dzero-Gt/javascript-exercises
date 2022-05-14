192ef8e744827c23d38fbe16200708aef4975807
Dzero-GT <dillongustafson2002@gmail.com>
2022-05-14 16:58:53 -0500

    exercise 2 complete

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,14 @@
-const repeatString = function() {
-
+const repeatString = function(text, n) {
+let results = "";
+if (n >= 0){
+    for (let i = 0; i < n; i++){
+        results += text;
+    }
+}
+else {
+    results = "ERROR";
+}
+return results;
 };
 
 // Do not edit below this line
