8dadd2ac470d680db9050b15e862a85affa41556
Dzero-GT <dillongustafson2002@gmail.com>
2022-05-18 18:51:32 -0500

    exercise 12 complete

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,14 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+   const oldestList = people.sort((a,b) => age(a) > age(b) ? -1 : 1); 
+   return people[0];
+}
 
-};
+function age(person){
+    if (!person.yearOfDeath){
+        person.yearOfDeath = new Date().getFullYear();
+    }
+    return (person.yearOfDeath - person.yearOfBirth);
+}
 
 // Do not edit below this line
 module.exports = findTheOldest;
