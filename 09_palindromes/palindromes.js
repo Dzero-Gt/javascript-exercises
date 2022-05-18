14db5390cce5595cd7bc0eef46aeb070df88be9e
Dzero-GT <dillongustafson2002@gmail.com>
2022-05-17 23:05:12 -0500

    use filter to finish

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,15 @@
-const palindromes = function () {
-
+const palindromes = function (a) {
+    let palindrome = a.split('');
+    palindrome.reverse();
+    let palin = '';
+    for (let i = 0; i < palindrome.length; i++){
+        palin += palindrome[i];
+    }
+    return palin === a;
 };
 
 // Do not edit below this line
 module.exports = palindromes;
+
+
+//use filter
