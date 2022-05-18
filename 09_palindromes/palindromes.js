666cbf9b32f1c0a90005c123f38a5bfd1c9fa6b6
Dzero-GT <dillongustafson2002@gmail.com>
2022-05-18 16:25:27 -0500

    exercise 9 complete

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,11 +1,23 @@
 const palindromes = function (a) {
-    let palindrome = a.split('');
-    palindrome.reverse();
-    let palin = '';
-    for (let i = 0; i < palindrome.length; i++){
-        palin += palindrome[i];
+    const aSplit = a.split('');
+    const palinSplit = a.split('');
+    palinSplit.reverse();
+    const aFilter = aSplit.filter(character => character.toLowerCase() != character.toUpperCase());
+    const palinFilter = palinSplit.filter(character => character.toLowerCase() != character.toUpperCase());
+    let aFinal = '';
+    let palinFinal = '';
+    let temp = '';
+    for (let i = 0; i < aFilter.length; i++){
+        temp = aFilter[i].toLowerCase();
+        aFinal += temp;
     }
-    return palin === a;
+    for (let i = 0; i < palinFilter.length; i++){
+        temp = palinFilter[i].toLowerCase();
+        palinFinal += temp;
+    }
+    console.log(aFinal);
+    console.log(palinFinal);
+    return (aFinal === palinFinal);
 };
 
 // Do not edit below this line
