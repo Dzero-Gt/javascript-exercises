9880c6767aaeb0cb8a96b8afd8bcab6257697561
Dzero-GT <dillongustafson2002@gmail.com>
2022-05-18 18:21:50 -0500

    exercise 10 complete

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,18 @@
-const fibonacci = function() {
+const fibonacci = function(place) {
+    if (place < 0){ return 'OOPS'}
+    place = +place;
 
+    let a = 0;  //trailing number
+    let b = 1;  //leading number
+    let temp = 0;
+    let sum = 1;
+    for (let i = 1; i < place; i++){
+        sum = a + b;
+        temp = sum;
+        a = b;
+        b = sum;
+    }
+    return sum;
 };
 
 // Do not edit below this line
